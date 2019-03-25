49fc8789ad45576fe1d8b4388348a62e57eabbf1
Kreisel <csas3559@uibk.ac.at>
2019-03-25 20:35:43 +0100

    8/10 String Interpolation

--- workload/3/main.js
+++ workload/3/main.js
@@ -1,3 +1,3 @@
-let favoriteAnimal = "rhino";
-
-console.log("My favorite animal: " + favoriteAnimal);
+var myName = 'Alex'
+var myCity = 'Innsbruck'
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`)

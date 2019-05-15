ba5cc304698aa792bab70dc3c240e06d2b7f350b
Kreisel <csas3559@uibk.ac.at>
2019-05-15 18:38:19 +0200

    Neu

--- wien/main.js
+++ wien/main.js
@@ -69,4 +69,85 @@ karte.addControl(new L.Control.Fullscreen());
 
 karte.setView([48.208333, 16.373056], 12);
 
+
+
+
 // die Implementierung der Karte startet hier
+const url = 'https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SPAZIERPUNKTOGD &srsName=EPSG:4326&outputFormat=json'
+
+function makeMarker(feature, latlng) {
+    const fotoIcon = L.icon({
+            iconUrl: 'http://www.data.wien.gv.at/icons/sehenswuerdigogd.png',
+            iconSize: [23, 23]
+        });
+        const sightMarker = L.marker(latlng, {
+            // L. --> Leaflat bibliothek
+            icon: fotoIcon
+    });
+    sightMarker.bindPopup(`
+        <h3>${feature.properties.NAME}</h3>
+        <p>${feature.properties.BEMERKUNG}</p>
+        <hr>
+        <footer><a target= "blank" href="${feature.properties.WEITERE_INF}">Weblink</a></footer>
+`);
+return sightMarker;
+}
+
+
+async function loadSights(url) {
+    const clusterGruppe = L.markerClusterGroup();
+    const antwort = await fetch(url);
+    const sightsData = await antwort.json();
+    const geoJson = L.geoJson(sightsData, {
+        pointToLayer: makeMarker
+    });
+    clusterGruppe.addLayer(geoJson);
+    karte.addLayer(clusterGruppe);
+    layerControl.addOverlay(clusterGruppe, "Sehenswürdigkeiten");
+
+    const suchFeld = new L.Control.Search({
+        layer: clusterGruppe,
+        propertyName: "NAME",
+        zoom: 17,
+        initial: false
+    });
+    karte.addControl(suchFeld);
+
+
+    const massstab = L.control.scale({
+        imperial: true,  // keine Variable, kommt von der Dokumentation
+        metric: false
+    });
+    karte.addControl(massstab);
+}
+
+const wege = 'https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SPAZIERLINIEOGD &srsName=EPSG:4326&outputFormat=json'
+
+function linienPopup(feature, layer) {
+    console.log(feature);
+    const popup = `
+    <h3>${feature.properties.NAME}</h3>
+    <p><a href="${feature.properties.WEITERE_INF}">Weblink</a></p>
+    `;
+    layer.bindPopup(popup);
+}
+
+async function loadWege(wegeUrl) {
+    const antwort = await fetch(wegeUrl);
+    const wegeData = await antwort.json();
+    console.log(wegeData);
+    const wegeJson = L.geoJson (wegeData, {
+        style: function() {
+            return {
+                color: "green"
+            };
+        },
+        onEachFeature: linienPopup
+    });
+    karte.addLayer(wegeJson);
+    layerControl.addOverlay(wegeJson, "Spazierwege");
+    
+}
+
+loadWege(wege);
+
